3c65f9a57564318da2831ce12fc438798f108aba
Alan Chung <alanchung2k2@gmail.com>
2025-07-15 00:26:39 -0400

    implement training with bigram model

--- train.py
+++ train.py
@@ -1,7 +1,49 @@
 import torch
 from languageModel import LanguageModel
+from bigram import BigramModel
+
+def getBatch(type):
+  data = trainData if type == 'train' else valData # data based on type
+  # list of batchSize random int between 0 to len(data) - blocksize (indices of data to indicate start of a block)
+  indices = torch.randint(len(data) - blockSize, (batchSize, ))
+  # these are all batchSize list of blockSize lists
+  x = torch.stack([data[idx: idx+blockSize] for idx in indices]) # list of list of context in the chunk (current element and all preceding element is the context)
+  y = torch.stack([data[idx+1:idx+blockSize+1] for idx in indices]) # list of list of target in the chunk (current element is the target)
+  x, y = x.to(device), y.to(device)
+  return x, y
+
+@torch.no_grad()
+def estimateLoss():
+  out = {}
+  model.eval()
+  for split in ['train', 'val']:
+    losses = torch.zeros(evalIters)
+    for k in range(evalIters):
+      X, Y = getBatch(split)
+      logits, loss = model(X, Y)
+      losses[k] = loss.item()
+    out[split] = losses.mean()
+  model.train()
+  return out
 
 if __name__ == '__main__':
+  torch.manual_seed(1337) # set seed for consistency
+
+  # hyperparameters
+  # train in chunks for efficiency
+  batchSize = 32 # independent chunks to process in parallel (GPU efficient)
+  # context-target based chunk training: the chunk contains information for every element in the chunk,
+  # that element can act as a target and everything preceding can act as the context. So it contains each
+  # element's positional information (context (as low as one characater) -> target)
+  # Allow the model to see many different context sizes
+  blockSize = 8 # maximum context length (chunk length) (that the model will be used to)
+  epochs = 3000
+  printInterval = 100
+  learningRate = 1e-3
+  device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+  evalIters = 200
+  maxNewTokens = 500
+
   # read in the file (1,000,000 characters)
   with open('input.txt', 'r', encoding='utf-8') as file:
     text = file.read()
@@ -16,13 +58,30 @@ if __name__ == '__main__':
   split = int(0.9 * len(data)) # first part of the data will be train then rest of it will be validation
   trainData = data[:split]
   valData = data[split:]
+  
+  model = BigramModel(lm.vocabSize).to(device)
+  # optimizer: method of updating the parameters using the gradients, ADAM (adaptive learning rate)
+  optimizer = torch.optim.AdamW(model.parameters(), lr=learningRate)
 
-  # train in chunks for efficiency
-  batchSize = 4 # independent chunks to process in parallel (GPU efficient)
-  # context-target based chunk training: the chunk contains information for every element in the chunk
-  # that element can act as a target and everything preceding can act as the context. So it contains each
-  # elemtent's positional information (context (as low as one characater) -> target)
-  # Allow the model to see many different context sizes
-  blockSize = 8 # maximum context length (chunk length) (that the model will be used to)
+  for epoch in range(epochs):
+    
+    # every printInterval we print out the losses on train and validation sets
+    if epoch % printInterval == 0:
+      losses = estimateLoss()
+      print(f'epoch {epoch + 1}/{epochs}, train loss={losses['train'].item():.4f}, val loss={losses['val']:.4f}')
 
-  torch.manual_seed(1337) # set seed for consistency
+    # get batches of data
+    xBatch, yBatch = getBatch('train')
+    # zero out gradients from previous epoch
+    optimizer.zero_grad(set_to_none=True)
+
+    # forward pass: evaluate the logits (prediction scores) and the loss
+    logits, loss = model(xBatch, yBatch)
+
+    # backward step
+    loss.backward() # calculate back propagation (gradients from all the parameters)
+    optimizer.step() # optimization step: update parameters using the gradients from previous step
+
+  # generate from the model
+  context = torch.zeros((1, 1), dtype=torch.long, device=device) # feed the new line character "\n" (0) as the starting sequence/context
+  print(lm.decode(model.generate(context, maxNewTokens)[0].tolist())) # generate from the initial context get the first batch and decode it
